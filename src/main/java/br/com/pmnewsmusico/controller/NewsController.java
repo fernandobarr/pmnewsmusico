a058c4d9f15b81bd3e58f821255cef2ceb17ae7a
Fernando Justino de Barros <fernandobarr@gmail.com>
2022-01-06 17:27:32 -0300

    teste 3

--- src/main/java/br/com/pmnewsmusico/controller/NewsController.java
+++ src/main/java/br/com/pmnewsmusico/controller/NewsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/news")
+@RequestMapping("")
 public class NewsController {
 		
 		@SuppressWarnings("deprecation")
