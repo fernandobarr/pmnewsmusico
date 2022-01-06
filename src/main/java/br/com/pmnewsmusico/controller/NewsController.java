6787663eed5f35ee9c38acc5a5e14bdbcca51e62
Fernando Justino de Barros <fernandobarr@gmail.com>
2022-01-06 17:11:52 -0300

    teste

---
+++ src/main/java/br/com/pmnewsmusico/controller/NewsController.java
@@ -0,0 +1,20 @@
+package br.com.pmnewsmusico.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/news")
+public class NewsController {
+		
+		@SuppressWarnings("deprecation")
+		@GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+		public ResponseEntity list(){
+			return ResponseEntity.ok("Fernando");
+		}	
+		
+		
+}
